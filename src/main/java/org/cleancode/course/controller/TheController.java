fc14cc2f299e4f72ad86c58f0e15b7a4142c6dce
David Martinez Sepulveda <davidmartinezsepulveda@gmail.com>
2021-11-04 21:33:17 +0100

    duplicateMethod

--- src/main/java/org/cleancode/course/controller/TheController.java
+++ src/main/java/org/cleancode/course/controller/TheController.java
@@ -2,6 +2,7 @@ package org.cleancode.course.controller;
 
 import org.cleancode.course.service.TheService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class TheController {
     @GetMapping("/getThem")
     public List<String[]> getThem() {
         return theService.getThem();
+    }
+
+    @GetMapping("duplicateOneAndReturnThemAll/{i}")
+    public List<String[]> duplicateOneAndReturnThemAll(@PathVariable int i) {
+        return theService.duplicateOneAndReturnThemAll(i);
     }
 }
--- src/main/java/org/cleancode/course/service/TheService.java
+++ src/main/java/org/cleancode/course/service/TheService.java
@@ -8,11 +8,15 @@ public class TheService {
 
     private int d; // Elapsed time in days
 
-    private List<String[]> theList = Arrays.asList(
-            new String[]{"1", "7 estrategias para ganar a las chapas", "Lorem ipsum...", "Arturo González", "3"},
-            new String[]{"2", "Descubre con este cuestionario si sufres del síndrome 'me gusta mas la cama que ir a trabajar'", "Lorem ipsum...", "Pedro Ramírez", "2"},
-            new String[]{"3", "Cómo me hice rico escribiendo posts sobre cómo me hice rico", "Lorem ipsum...", "Juan", "3"},
-            new String[]{"4", "Esto es un borrador", "...", "Juan", "1"});
+    private List<String[]> theList = new ArrayList<>();
+
+    public TheService() {
+        this.theList.addAll(Arrays.asList(
+                new String[]{"1", "7 estrategias para ganar a las chapas", "Lorem ipsum...", "Arturo González", "3"},
+                new String[]{"2", "Descubre con este cuestionario si sufres del síndrome 'me gusta mas la cama que ir a trabajar'", "Lorem ipsum...", "Pedro Ramírez", "2"},
+                new String[]{"3", "Cómo me hice rico escribiendo posts sobre cómo me hice rico", "Lorem ipsum...", "Juan", "3"},
+                new String[]{"4", "Esto es un borrador", "...", "Juan", "1"}));
+    }
 
     public List<String[]> getThemAll() {
         return theList;
@@ -26,5 +30,18 @@ public class TheService {
                 list1.add(x);
 
             return list1;
+    }
+
+    public List<String[]> duplicateOneAndReturnThemAll(int i) {
+        String[] copy = new String[theList.get(i).length];
+        copyStrings(theList.get(--i), copy);
+        theList.add(copy);
+        return theList;
+    }
+
+    public void copyStrings(String a1[], String a2[]) {
+        for (int i = 0; i < a1.length; i++) {
+            a2[i] = a1[i];
+        }
     }
 }
